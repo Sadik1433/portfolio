11e5e2505a460bbddc186c45f78955e34202668a
sadik1433 <shaiksiddik98365@gmail.com>
2025-02-26 18:31:17 +0530

    second

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,16 @@
 import "./App.css";
 // import About from "./About";
-// import ProfileCard from "./ProfileCard";
+import ProfileCard from "./ProfileCard";
 // import Contact from "./Contact";
 import Navbar from "./Navbar";
-import About from "./About"
+
 
 const App = () => {
   return (
     <>
       <Navbar />
-      <div>
-        <About /> 
-        {/* <ProfileCard />
-        <About />
-        <Contact /> */}
+      <div> 
+        <ProfileCard />
       </div>
     </>
   );
